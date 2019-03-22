b0a7da432ab2a0c0081ca7f5472f6e8c4452d991
Denis Di Fazio <denisdifazio@gmail.com>
2019-03-22 00:03:44 -0300

    add plug-vim download

--- init.sh
+++ init.sh
@@ -3,6 +3,10 @@
 # get the absolute path of our dotfiles repository
 DIR=$(cd `dirname $0` && pwd)
 
+# download and install plug-vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 # overwrite existing symlinks if necessary
 ln -sf $DIR/.bashrc ~/.bashrc
 ln -sf $DIR/.inputrc ~/.inputrc
